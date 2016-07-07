790c551765cc9024ae4305610ee147d094d1cd34
johanna <jbjorklund_@hotmail.com>
2016-07-07 10:51:23 +0200

    Hej!

--- syncMedJohanna/syncMedJohanna/Program.cs
+++ syncMedJohanna/syncMedJohanna/Program.cs
@@ -11,6 +11,7 @@ namespace syncMedJohanna
         static void Main(string[] args)
         {
             Console.WriteLine("Hej Johanna :)");
+            Console.WriteLine("Hej Carina!!");
         }
     }
 }
